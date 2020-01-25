2f08d8370001169ac17e7ed5884517bafffa1faf
Mohammad Butt <mbutt@student.42.us.org>
2020-01-24 21:52:08 -0800

    Need to get more os info

--- mbutt/OsInfoModule.cpp
+++ mbutt/OsInfoModule.cpp
@@ -6,7 +6,7 @@
 /*   By: mbutt <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/24 16:05:42 by mbutt             #+#    #+#             */
-/*   Updated: 2020/01/24 20:46:30 by mbutt            ###   ########.fr       */
+/*   Updated: 2020/01/24 21:47:06 by mbutt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,15 +50,33 @@ std::string OsInfoModule::getReleaseVersion(void)
 std::string OsInfoModule::getVersionDate(void)
 {
 	std::string fullString = _info.version;
-	std::string newString = fullString.substr(30, 28);
-	
-//	return(_info.version);
+	std::string newString = fullString.substr(30, 28);	
 	return(newString);
 }
 
 std::string OsInfoModule::getMachine(void)
 {
 	return(_info.machine);
+}
+
+std::string OsInfoModule::getCpuInfo(void)
+{
+	char cpuInfo[64];
+	size_t size = sizeof(cpuInfo);
+	if(sysctlbyname("machdep.cpu.brand_string", &cpuInfo, &size, NULL, 0) < 0)
+	{
+		perror("sysctl");
+	}
+//	std::cout << "cores: " << std::thread::hardware_concurrency() << std::endl;
+	return(cpuInfo);
+}
+
+unsigned int OsInfoModule::getCpuCores(void)
+{
+	unsigned int numberOfCores;
+
+	numberOfCores = std::thread::hardware_concurrency();
+	return(numberOfCores);
 }
 
 int main(void)
@@ -73,5 +91,24 @@ int main(void)
 	std::cout << "release version: " << info.getReleaseVersion() << std::endl;
 	std::cout << "version: " << info.getVersionDate() << std::endl;
 	std::cout << "Machine: " << info.getMachine() << std::endl;
+	std::cout << "CPU: " << info.getCpuInfo() << std::endl;
+	std::cout << "Cores: " << info.getCpuCores() << std::endl;
+
+	    int mib[2];
+    int64_t physical_memory;
+    size_t length;
+
+    // Get the Physical memory size
+    mib[0] = CTL_HW;
+    mib[1] = HW_MEMSIZE;
+    length = sizeof(int64_t);
+   
+	std::cout << "mib[0]: " << mib[0] << std::endl;
+	std::cout << "mib[1]: " << mib[1] << std::endl;
+   	sysctl(mib, 2, &physical_memory, &length, NULL, 0);
+	std::cout << "mib[0]: " << mib[0] << std::endl;
+	std::cout << "mib[1]: " << mib[1] << std::endl;
+	std::cout << "physical_memory: " << physical_memory << std::endl;
+	std::cout << "length: " << length << std::endl;
 }
 
--- mbutt/OsInfoModule.hpp
+++ mbutt/OsInfoModule.hpp
@@ -6,7 +6,7 @@
 /*   By: mbutt <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/24 16:05:14 by mbutt             #+#    #+#             */
-/*   Updated: 2020/01/24 20:46:33 by mbutt            ###   ########.fr       */
+/*   Updated: 2020/01/24 21:47:03 by mbutt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,9 @@
 //#include <abc.h>
 #include <iostream>
 #include <sys/utsname.h>
+#include <sys/sysctl.h>
+#include <thread>
+
 
 class OsInfoModule
 {
@@ -35,16 +38,10 @@ class OsInfoModule
 		std::string getReleaseVersion(void);
 		std::string getVersionDate(void);
 		std::string getMachine(void);
+		std::string getCpuInfo(void);
+		unsigned int getCpuCores(void);
 	private:
 		struct utsname _info;
-//		std::string userName;
-//		std::string sysName;
-//		std::string nodeName;
-//		std::string release;
-//		std::string version;
-//		std::string machine;
-//		std::string domainName;
-//		void setSystemInfo(void);
 };
 
 #endif
