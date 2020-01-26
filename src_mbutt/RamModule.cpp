948a034c90892e4f3805a52dc59a47217fd2f379
Mohammad Butt <mbutt@student.42.us.org>
2020-01-26 11:02:10 -0800

    Finished Ram Module

--- src_mbutt/RamModule.cpp
+++ src_mbutt/RamModule.cpp
@@ -6,7 +6,7 @@
 /*   By: mbutt <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/25 15:57:35 by mbutt             #+#    #+#             */
-/*   Updated: 2020/01/25 22:26:54 by mbutt            ###   ########.fr       */
+/*   Updated: 2020/01/26 11:01:27 by mbutt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,7 +43,7 @@ std::string RamModule::getRamUsed(void)
 }
 */
 
-int64_t RamModule::getPhysicalMemoryBytes(void)
+std::string RamModule::getPhysicalMemoryBytes(void)
 {
 	int mib[2];
 	int64_t physicalMemory;
@@ -53,55 +53,94 @@ int64_t RamModule::getPhysicalMemoryBytes(void)
 	mib[1] = HW_MEMSIZE;
 	length = sizeof(int64_t);
 	sysctl(mib, 2, &physicalMemory, &length, NULL, 0);
-	return(physicalMemory);
+	return(std::to_string(physicalMemory));
 }
 
-int RamModule::getPhysicalMemoryMbytes(void)
+std::string RamModule::getPhysicalMemoryMbytes(void)
 {
 	int mib[2];
 	int64_t physicalMemory;
+	int64_t	physicalMemoryInMbytes;
 	size_t length;
 
 	mib[0] = CTL_HW;
 	mib[1] = HW_MEMSIZE;
 	length = sizeof(int64_t);
 	sysctl(mib, 2, &physicalMemory, &length, NULL, 0);
-
-	return(physicalMemory / (1024 * 1024));
+	physicalMemoryInMbytes = (physicalMemory / (1024 * 1024));
+	
+	return(std::to_string(physicalMemoryInMbytes));
+//	return(physicalMemory / (1024 * 1024));
 }
 
-int RamModule::getPhysicalMemoryGbytes(void)
+std::string RamModule::getPhysicalMemoryGbytes(void)
 {
 	int mib[2];
 	int64_t physicalMemory;
+	int64_t physicalMemoryInGbytes;
 	size_t length;
 
 	mib[0] = CTL_HW;
 	mib[1] = HW_MEMSIZE;
 	length = sizeof(int64_t);
 	sysctl(mib, 2, &physicalMemory, &length, NULL, 0);
+	physicalMemoryInGbytes = (physicalMemory / (1024 * 1024 * 1024));
+	
+	return(std::to_string(physicalMemoryInGbytes));
+//	std::string memoryGbytesString;
+//	memoryGbytesString = std::to_string((physicalMemory / (1024 * 1024 * 1024)));
 
-	return(physicalMemory / (1024 * 1024 * 1024));
+//	std::cout << "Physical Memory String" << std::endl;
+//	std::cout << memoryGbytesString << std::endl;
+//	return(physicalMemory / (1024 * 1024 * 1024));
 }
 
 std::string RamModule::getPhysicalMemoryUsed(std::string topInfo)
 {
-	std::cout << topInfo.substr(9, 5) << std::endl;
-	return("PhysicalMemoryUsed");
+//	std::cout << "Physical memory used" << std::endl;
+//	std::cout << topInfo.substr(9, 5) << std::endl;
+	return(topInfo.substr(9, 5));
+}
+
+std::string RamModule::getPhysicalMemoryUnUsed(std::string topInfo)
+{
+//	std::cout << "Physcial memory used" << std::endl;
+//	std::cout << topInfo.substr(35, 5) << std::endl;
+	return(topInfo.substr(35, 5));
 }
 
 void RamModule::update(void)
 {
-	std::cout << topinfo.substr(35, 5) << std::endl;
-	return("This");
-
+//	std::cout << topinfo.substr(35, 5) << std::endl;
+//	return("This");
 }
 
+std::vector<std::string> RamModule::getInfo(std::string topInfo[])
+{
+
+//	std::cout << "Inside RamModule::getInfo" << std::endl;
+
+	std::vector<std::string> ramVector;
+
+	
+	ramVector.push_back(getPhysicalMemoryUsed(topInfo[6]));
+	ramVector.push_back(getPhysicalMemoryUnUsed(topInfo[6]));
+	ramVector.push_back(getPhysicalMemoryGbytes());
+	ramVector.push_back(getPhysicalMemoryMbytes());
+	ramVector.push_back(getPhysicalMemoryBytes());
+
+//	std::cout << getPhysicalMemoryGbytes() << std::endl;
+//	std::cout << getPhysicalMemoryMbytes()[0] << std::endl;
+//	std::cout << getPhysicalMemoryBytes()[0] << std::endl;
+	return(ramVector);
+}
+
+/*
 std::vector<std::string> RamModule::updateRamModule(std::string topInfo)
 {
 
 }
-
+*/
 
 /*
 int main(void)
