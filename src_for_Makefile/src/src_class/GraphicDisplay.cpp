fe49dc993cc28c52347b599b720652af31592249
Mohammad Butt <mbutt@student.42.us.org>
2020-01-26 21:29:58 -0800

    moved files

---
+++ src_for_Makefile/src/src_class/GraphicDisplay.cpp
@@ -0,0 +1,76 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   GraphicDisplay.cpp                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jchiang- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/01/26 15:37:48 by jchiang-          #+#    #+#             */
+/*   Updated: 2020/01/26 21:23:59 by mbutt            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../src_include/GraphicDisplay.hpp"
+
+GraphicDisplay::GraphicDisplay(void) {
+
+    _font.loadFromFile("fonts/arial.ttf");
+
+}
+
+GraphicDisplay::GraphicDisplay(const GraphicDisplay & copy) { *this = copy; }
+GraphicDisplay &GraphicDisplay::operator = (const GraphicDisplay & inputClass) {
+
+	(void)inputClass;
+	return *this;
+}
+GraphicDisplay::~GraphicDisplay(void) { }
+
+void GraphicDisplay::render(ManagerModule * mn) {
+	
+    sf::RenderWindow window(sf::VideoMode(1000, 800), "SFML window");
+    while (window.isOpen())
+    {
+        sf::Event event;
+		int i = 0;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+		mn->update();
+		mn->allModuleInfo();
+        window.clear();
+		for (std::vector<std::string>::iterator it = mn->tmInfo.begin(); it != mn->tmInfo.end(); ++it) {
+    		sf::Text text(*it + "\n", _font, 25);
+			text.setPosition(20, 20 + i);
+			i += 30;
+      	 	window.draw(text);
+		}
+		for (std::vector<std::string>::iterator it = mn->cpuInfo.begin(); it != mn->cpuInfo.end(); ++it) {
+    		sf::Text text(*it + "\n", _font, 25);
+			text.setPosition(20, 20 + i);
+			i += 30;
+      	 	window.draw(text);
+		}
+		for (std::vector<std::string>::iterator it = mn->osInfo.begin(); it != mn->osInfo.end(); ++it) {
+    		sf::Text text(*it + "\n", _font, 25);
+			text.setPosition(20, 20 + i);
+			i += 30;
+      	 	window.draw(text);
+		}
+		for (std::vector<std::string>::iterator it = mn->nwInfo.begin(); it != mn->nwInfo.end(); ++it) {
+    		sf::Text text(*it + "\n", _font, 25);
+			text.setPosition(20, 20 + i);
+			i += 30;
+      	 	window.draw(text);
+		}
+		for (std::vector<std::string>::iterator it = mn->rmInfo.begin(); it != mn->rmInfo.end(); ++it) {
+    		sf::Text text(*it + "\n", _font, 25);
+			text.setPosition(20, 20 + i);
+			i += 30;
+      	 	window.draw(text);
+		}
+        window.display();
+    }
+}
